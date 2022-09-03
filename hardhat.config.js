31cd3f158ccd9600fa2f850bdb9a7b1200d47fb4
mooujarrar <mohyiddine1997gmail.com>
2022-09-03 15:50:25 +0200

    enh: configuration of redhat, using goerli test network

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,16 @@
 /** @type import('hardhat/config').HardhatUserConfig */
+require("@nomiclabs/hardhat-ethers");
+const { API_URL, PRIVATE_KEY } = process.env;
+require('dotenv').config()
+
 module.exports = {
   solidity: "0.8.9",
+  defaultNetwork: "goerli",
+  networks: {
+     hardhat: {},
+     goerli: {
+        url: API_URL,
+        accounts: [`0x${PRIVATE_KEY}`]
+     }
+  },
 };
